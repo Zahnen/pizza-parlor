542d919bce4b5dd3b93778b832da657d78afae99
Zahnen Garner <zahnen@gmail.com>
2020-11-06 09:01:11 -0800

    add addToppings prototype, pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function Pizza (size, toppings) {
   this.toppings = [];
 }
 
+Pizza.prototype.addToppings = function(toppings) {
+  (this.toppings).push(toppings)
+}
+
 /*
 Describe: Pizza ()
 Test: should correctly create an instance of Pizza
@@ -10,4 +14,11 @@ Code: let size = "small";
 Code: let pizza1 = new Pizza("small");
 Code: let result = newPizza.size;
 Expect: result.toEqual("small");
+
+Describe: addToppings()Test: should add toppings to appropriate array within Pizza
+Code: let size = "small";
+Code: let pizza1 = new Pizza("small");
+Code: pizza1.addToppings("cheese");
+Code: let result = pizza1.toppings;
+Expect: result.toEqual("cheese");
 */
