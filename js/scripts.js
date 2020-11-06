bc4e1a6db91376eb8c0889ceaecbb5131607e4d0
Zahnen Garner <zahnen@gmail.com>
2020-11-06 10:10:41 -0800

    add calcPrice prototype, pass test 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,22 @@ Pizza.prototype.addToppings = function(toppings) {
   (this.toppings).push(toppings)
 }
 
+Pizza.prototype.calcPrice = function(size, toppings) {
+  let total = 10
+  if (this.size === "medium") {
+    total += 4;
+  } else if (this.size === "large") {
+    total += 6;
+  } else {
+    total += 0;
+  }
+  for (let i=0; i < this.toppings.length; i++) {
+    total +=1.5;
+  }
+  return total
+}
+
+
 /*
 Describe: Pizza ()
 Test: should correctly create an instance of Pizza
@@ -21,4 +37,12 @@ Code: let pizza1 = new Pizza("small");
 Code: pizza1.addToppings("cheese");
 Code: let result = pizza1.toppings;
 Expect: result.toEqual("cheese");
+
+Describe: calcPrice()
+Test: should calculate a price based on Pizza size and number of toppings
+Code: let size = "small";
+Code: let pizza1 = new Pizza("small");
+Code: pizza1.addToppings("cheese", "olives");
+Code: let result = pizza1.calcPrice();
+Expect: result.toEqual("18");
 */
