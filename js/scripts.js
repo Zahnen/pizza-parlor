d8d1075ca9d97a7cab66251f9f630c98be567451
Zahnen Garner <zahnen@gmail.com>
2020-11-06 08:54:05 -0800

    add pizza constructor, pass test 1

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Pizza (size, toppings) {
+  this.size = size;
+  this.toppings = [];
+}
+
+/*
+Describe: Pizza ()
+Test: should correctly create an instance of Pizza
+Code: let size = "small";
+Code: let pizza1 = new Pizza("small");
+Code: let result = newPizza.size;
+Expect: result.toEqual("small");
+*/
